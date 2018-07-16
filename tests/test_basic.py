e9383aa28e93dbe914235e6b4f49fbdccfebcf9a
stevenshuang <stevenshuang521@gmail.com>
2018-07-17 00:07:16 +0800

    使用unittest

---
+++ tests/test_basic.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+import unittest
+from flask import current_app
+from app import create_app, db
+
+
+class BasicTestCase(unittest.TestCase):
+
+    def setUp(self):
+        # 创建一个测试环境
+        self.app  = create_app('testing')
+        self.app_context = self.app.app_context()
+        self.app_context.push()
+        db.create_all()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+        self.app_context.pop()
+
+    def test_app_exists(self):
+        self.assertFalse(current_app is None)
+
+    def test_app_testing(self):
+        self.assertTrue(current_app.config['TESTING'])
