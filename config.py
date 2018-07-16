0440a102a0a721ed28f0c059637066e250fcdbec
stevenshuang <stevenshuang521@gmail.com>
2018-07-17 00:07:32 +0800

    cfg

---
+++ config.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+import os
+
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config:
+
+    SECRET_KEY = 'Coco'
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+    FLASKY_MAIL_SUBJECT_PREFIX = '[flasky]'
+    FLASKY_MAIL_SENDER = 'Flasky Admin <shuangr521@gmail.com>'
+    FLASKY_ADMIN = 'Coco'
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    MAIL_SERVER = 'stmp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = 'shuangr521@gmail.com'
+    MAIl_PASSWORD = '123456'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:Zs935809546@localhost:3306/SRDRA'
+
+
+class TestConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:Zs935809546@localhost:3306/SRDRA'
+
+
+class ProductionConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:Zs935809546@localhost:3306/SRDRA'
+
+
+config = {
+    'development': DevelopmentConfig,
+    'testing': TestConfig,
+    'production': ProductionConfig,
+    'default': DevelopmentConfig
+}
