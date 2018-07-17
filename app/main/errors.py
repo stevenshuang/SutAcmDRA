6f13b87eef6f93528a442b0fcbf8b68a82b10c68
stevenshuang <stevenshuang521@gmail.com>
2018-07-18 00:37:45 +0800

    404page

---
+++ app/main/errors.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+from flask import request, jsonify, render_template
+from . import main
+
+
+@main.app_errorhandler(404)
+def page_not_fount(e):
+    if request.accept_mimetypes.accept_json and not request.accept_mimetypes.accept_html:
+        response = jsonify({'error': 'not found'})
+        response.status_code = 404
+        return response
+    return render_template('404.html'), 404
