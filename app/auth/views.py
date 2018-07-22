07ef375822d2edb58e16af00015152461bec573e
stevenshuang <stevenshuang521@gmail.com>
2018-07-22 10:37:26 +0800

    引入logger

--- app/auth/views.py
+++ app/auth/views.py
@@ -24,7 +24,7 @@ def login():
             logger.info('用户不存在')
         if user is not None and user.verify_password(passwd):
             login_user(user)
-            return redirect(request.args.get('next') or url_for('main.index'))
+            return redirect(request.args.get('next') or url_for('main.index', ))
         flash('Invald Username Or Password')
 
 
