124907d036e131f3490150380b562ae72cc4064c
stevenshuang <stevenshuang521@gmail.com>
2018-07-23 14:29:50 +0800

    去掉在框架中带数据打后台, 整合api+ajax

--- app/main/views.py
+++ app/main/views.py
@@ -9,6 +9,7 @@ from flask import g, abort, make_response
 from flask_login import login_user
 from flask_login import logout_user
 from flask_login import current_user
+from flask_login import login_required
 
 from . import main
 from .. import db
@@ -20,17 +21,12 @@ from .. import logger
 
 @main.route('/', methods=['GET'])
 def index():
-    users = User.query.all()
-    users_name = [user.hdoj_username for user in users]
-    return render_template('base.html', user=users[0])
+
+    return render_template('base.html')
 
 
-@main.route('/profile/<username>')
-def profile(username):
-    user_name = current_user.hdoj_username
-    try:
-        user = User.query.get(hdoj_username=users_name)
-        return render_template('Homepage.html', user=user)
-    except Exception as e:
-        logger.info('User <> does not exists'.format(username))
-        return abort(make_response('404.html'))
+@main.route('/homepage')
+@login_required
+def profile():
+    # ajax去请求数据
+    return render_template('Homepage.html')
